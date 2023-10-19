e7ad109612ab4320ec2fa27d33ff95eceafeed16
relasts <126331504+relasts@users.noreply.github.com>
2023-10-19 10:39:31 +0300

    Add files via upload

--- index6.js
+++ index6.js
@@ -8,38 +8,49 @@ window.onload = function () {
     let type_tovar = document.getElementById("type_tovar");
     let garantia = document.getElementById("garantia");
     let e_sim = document.getElementById("e-sim");
-    let select_el = 0;
+    let errorh = document.getElementById("error").innerHTML;
+    let select_el = -1;
+    let error = "";
 
-    function price(sub) {
+    function isInts(x, type = 0){
+        if (error === "") {
+            if (/^\d+$/.test(x)) {
+                error = "";
+                return parseInt(x);
+            }
+
+            if (!type) {
+                error = "Неверно указана стоимость товара";
+            } else {
+                error = "Неверно указано количество товара";
+            }
+        }
+        return 0;
+    }
+
+    function price(type) {
         let sum = 0;
-        let csum = parseInt(count_inp.value);
+        let csum = isInts(count_inp.value, 1);
 
-        // if (/^\d+$/.test(dataPrice)) {
-        //     dataPrice = Number(dataPrice);
-        // } else {
-        //     error = "Неверно указана стоимость товара";
-        // }
-
-        if (sub === 0) {
-            let el_price = parseInt(tovar1.getAttribute("data-price"));
-            sum = el_price * csum;
+        if (type === 0) {
+            sum = isInts(tovar1.getAttribute("data-price")) * csum;
         }
-        if (sub === 1) {
+        if (type === 1) {
             let dataPriceEl = type_tovar.options[type_tovar.selectedIndex];
-            let dataPrice = dataPriceEl.getAttribute("data-price");
-            sum = parseInt(dataPrice) * csum;
+            sum = isInts(dataPriceEl.getAttribute("data-price")) * csum;
         }
-        if (sub === 2) {
-            sum = parseInt(tovar3.getAttribute("data-price")) * csum;
-
+        if (type === 2) {
             if (garantia.checked) {
-                sum += parseInt(garantia.getAttribute("data-price")) * csum;
+                sum = isInts(garantia.getAttribute("data-price")) * csum;
             }
             if (e_sim.checked) {
-                sum += parseInt(e_sim.getAttribute("data-price")) * csum;
+                sum = isInts(e_sim.getAttribute("data-price")) * csum;
             }
+            sum += isInts(tovar3.getAttribute("data-price")) * csum;
         }
         document.getElementById("price").innerHTML = sum + "₽";
+        document.getElementById("error").innerHTML = error;
+        error = "";
     }
 
     tovar1.addEventListener("click", function () {
@@ -66,7 +77,9 @@ window.onload = function () {
     });
 
     count_inp.addEventListener("input", function () {
-        price(select_el);
+        if (select_el !== -1) {
+            price(select_el);
+        }
     });
 
     garantia.addEventListener("click", function () {
