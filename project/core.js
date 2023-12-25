a9e016ccc1596b2a98206973f924bcf45c2cfd9e
QULVAR <127725086+QULVAR@users.noreply.github.com>
2023-12-25 19:35:48 +0300

    FAQ beta

--- project/core.js
+++ project/core.js
@@ -51,7 +51,88 @@ window.onload = function () {
         setTimer = setTimeout(() => { slicker(); }, 500);
     });
   
+  let FAQelement1 = document.getElementById("FAQ1li");
+  let FAQelement2 = document.getElementById("FAQ2li");
+  let FAQelement3 = document.getElementById("FAQ3li");
+  let FAQelement4 = document.getElementById("FAQ4li");
+  let FAQelement5 = document.getElementById("FAQ5li");
+  let FAQelement6 = document.getElementById("FAQ6li");
+  let FAQelement7 = document.getElementById("FAQ7li");
+  let FAQelement8 = document.getElementById("FAQ8li");
+  let FAQelement9 = document.getElementById("FAQ9li");
+  let FAQelement10 = document.getElementById("FAQ10li");
+  let FAQelement11 = document.getElementById("FAQ11li");
+  let FAQelement12 = document.getElementById("FAQ12li");
+  let FAQelements = [FAQelement1, FAQelement2, FAQelement3, FAQelement4, FAQelement5, FAQelement6, FAQelement7, FAQelement8, FAQelement9, FAQelement10, FAQelement11, FAQelement12];
   
-
+  function FAQ(id) {
+    for (i = 0; i < 12; i += 1) {
+      FAQelements[i].setAttribute("class", "passive");
+    }
+    FAQelements[id].setAttribute("class", "active");
+  }
+  
+  let btn_FAQ1 = document.getElementById("FAQ1");
+  btn_FAQ1.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ1.getAttribute("element"));
+  });
+  
+  let btn_FAQ2 = document.getElementById("FAQ2");
+  btn_FAQ2.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ2.getAttribute("element"));
+  });
+  
+  let btn_FAQ3 = document.getElementById("FAQ3");
+  btn_FAQ3.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ3.getAttribute("element"));
+  });
+  let btn_FAQ4 = document.getElementById("FAQ4");
+  btn_FAQ4.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ4.getAttribute("element"));
+  });
+  let btn_FAQ5 = document.getElementById("FAQ5");
+  btn_FAQ5.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ5.getAttribute("element"));
+  });
+  let btn_FAQ6 = document.getElementById("FAQ6");
+  btn_FAQ6.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ6.getAttribute("element"));
+  });
+  let btn_FAQ7 = document.getElementById("FAQ7");
+  btn_FAQ7.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ7.getAttribute("element"));
+  });
+  let btn_FAQ8 = document.getElementById("FAQ8");
+  btn_FAQ8.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ8.getAttribute("element"));
+  });
+  let btn_FAQ9 = document.getElementById("FAQ9");
+  btn_FAQ9.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ9.getAttribute("element"));
+  });
+  let btn_FAQ10 = document.getElementById("FAQ10");
+  btn_FAQ10.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ10.getAttribute("element"));
+  });
+  let btn_FAQ11 = document.getElementById("FAQ11");
+  btn_FAQ11.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ11.getAttribute("element"));
+  });
+  let btn_FAQ12 = document.getElementById("FAQ12");
+  btn_FAQ12.addEventListener("click", function (e) {
+    e.preventDefault();
+    FAQ(btn_FAQ12.getAttribute("element"));
+  });
     // 6LemVDkpAAAAAMTv_m7XcDbfvpIv1tYvD8prjJyK
 };
